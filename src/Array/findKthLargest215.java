594bb5dfd6e8aef5ee59974f169d051f2dcb1a0f
zoubaitao <zoubaitao@gmail.com>
2019-04-26 23:10:32 +0800

    edit findKthLargest215
    Signed-off-by: zoubaitao <zoubaitao@gmail.com>

--- src/Array/findKthLargest215.java
+++ src/Array/findKthLargest215.java
@@ -6,38 +6,40 @@ public class findKthLargest215 {
     @Test
     public void test1(){
         findKthLargest215 find = new findKthLargest215();
-        int[] nums = new int[]{3,2,1,5,6,4};
-        int k = find.findKthLargest(nums,3);
+        int[] nums = new int[]{3,2,3,1,2,4,5,5,6};
+        int k = find.findKthLargest(nums,4);
+        System.out.println(k);
     }
     public int findKthLargest(int[] nums, int k) {
         int left = 0;
-        int right = nums.length;
-        int index = - 1;
+        int right = nums.length - 1;
+        int index = -1;
         while(true){
             index = partition(nums,left,right);
-            if(index > k){
+            if(index > nums.length - k){
                 right = index - 1;
-            }else if(index < k){
+            }else if(index < nums.length - k){
                 left = index + 1;
 
             }else{
-                return nums[k];
+                return nums[index];
             }
         }
 
     }
 
-    private int  partition(int[] nums,int right,int left){
-        int pivot = nums[right];
-        int i = right - 1;
+    private int  partition(int[] nums,int left,int right){
+        int pivot = nums[left];
+        int i = left - 1;
         int j = right;
-        for(int k = right;j<nums.length;j++){
-            if(nums[k]>=pivot){
+        for(int k = left;k <= right;k++){
+            if(nums[k] <= pivot){
                 swap(nums,i + 1,k);
                 i ++;
             }
+
         }
-        swap(nums,right,i);
+        swap(nums,left,i);
         return i;
     }
     private void swap(int[] nums,int i,int j){
