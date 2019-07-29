440e7f839e633b6a1b0db5bd73a1eaaf54509b31
zoubaitao <zoubaitao@gmail.com>
2019-07-30 00:14:14 +0800

    add rangeBitwiseAnd201
    Signed-off-by: zoubaitao <zoubaitao@gmail.com>

---
+++ test/Array/rangeBitwiseAnd201Test.java
@@ -0,0 +1,23 @@
+package Array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class rangeBitwiseAnd201Test {
+    @Test
+    public void test1(){
+        rangeBitwiseAnd201 rangeBitWiseAnd201 = new rangeBitwiseAnd201();
+        int m = 5;
+        int n = 7;
+        int result = rangeBitWiseAnd201.rangeBitwiseAnd(m,n);
+        Assert.assertEquals(result,0);
+    }
+    @Test
+    public void test2(){
+        rangeBitwiseAnd201 rangeBitWiseAnd201 = new rangeBitwiseAnd201();
+        int m = 0;
+        int n = 1;
+        int result = rangeBitWiseAnd201.rangeBitwiseAnd(m,n);
+        Assert.assertEquals(result,0);
+    }
+}
