003a12de4b4b596901864446db2b9e0b7ee8237c
zoubaitao <zoubaitao@gmail.com>
2020-04-25 21:15:50 +0800

    optimize:CommonUnionFind和WeightedUnionFind
    Signed-off-by: zoubaitao <zoubaitao@gmail.com>

---
+++ src/unionfind/CommonUnionFind.java
@@ -0,0 +1,89 @@
+package unionfind;
+
+import java.util.Arrays;
+
+/**
+ * Weighted Union Find Algorithm
+ * https://opendsa-server.cs.vt.edu/ODSA/Books/Everything/html/UnionFind.html#the-union-find-problem
+ * two dimension array : https://stackoverflow.com/questions/49084312/union-find-in-a-2d-array-java
+ *
+ * @author baitao zou
+ * date 2020/03/08
+ */
+public class CommonUnionFind {
+    private int[] id;
+
+    private int[] size;
+
+    public CommonUnionFind(int num) {
+        id = new int[num];
+        for (int i = 0; i < num; i++) {
+            id[i] = i;
+        }
+        size = new int[num];
+        Arrays.fill(size, 1);
+    }
+
+    private int root(int i) {
+        while (i != id[i]) {
+            i = id[i];
+        }
+        return i;
+    }
+
+    public void union(int p, int q) {
+        int pid = root(p);
+        int qid = root(q);
+        id[qid] = pid;
+    }
+
+    /**
+     * 带权重的union算法
+     *
+     * @param p
+     * @param q
+     */
+    public void weightedUnion(int p, int q) {
+        int pid = root(p);
+        int qid = root(q);
+        if (size[pid] >= size[qid]) {
+            id[qid] = pid;
+            size[pid] += size[qid];
+        } else {
+            id[pid] = qid;
+            size[qid] += size[pid];
+        }
+    }
+
+    /**
+     * 路径压缩的union算法
+     *
+     * @param p
+     * @param q
+     */
+    public void pathCompressionUnion(int p, int q) {
+        int pid = pathCompressionRoot(p);
+        int qid = pathCompressionRoot(q);
+        if (size[pid] >= size[qid]) {
+            id[qid] = pid;
+            size[pid] += size[qid];
+        } else {
+            id[pid] = qid;
+            size[qid] += size[pid];
+        }
+    }
+
+    /**
+     * 路径压缩的find Root 方法
+     *
+     * @param i
+     * @return
+     */
+    private int pathCompressionRoot(int i) {
+        while (i != id[i]) {
+            id[i] = id[id[i]];
+            i = id[i];
+        }
+        return i;
+    }
+}
--- src/unionfind/WeightedUnionFind.java
+++ src/unionfind/WeightedUnionFind.java
@@ -1,30 +1,36 @@
 package unionfind;
 
-import java.util.Arrays;
-
 /**
- * Weighted Union Find Algorithm
- * https://opendsa-server.cs.vt.edu/ODSA/Books/Everything/html/UnionFind.html#the-union-find-problem
- * two dimension array : https://stackoverflow.com/questions/49084312/union-find-in-a-2d-array-java
- *
  * @author baitao zou
- * date 2020/03/08
+ * date 2020/04/01
  */
 public class WeightedUnionFind {
+    /**
+     * the parent of each id
+     */
     private int[] id;
 
+    /**
+     * the size of each the union tree
+     */
     private int[] size;
 
-    public WeightedUnionFind(int num) {
-        id = new int[num];
-        for (int i = 0; i < num; i++) {
-            id[i] = i;
+    private int group;
+
+    public WeightedUnionFind(int len) {
+        if (len <= 0) {
+            throw new IllegalArgumentException("the length must bigger than zero");
         }
-        size = new int[num];
-        Arrays.fill(size, 1);
+        group = len;
+        id = new int[len];
+        size = new int[len];
+        for (int i = 0; i < len; i++) {
+            id[i] = i;
+            size[i] = 1;
+        }
     }
 
-    private int root(int i) {
+    private int root(int i, int[] id) {
         while (i != id[i]) {
             i = id[i];
         }
@@ -32,58 +38,22 @@ public class WeightedUnionFind {
     }
 
     public void union(int p, int q) {
-        int pid = root(p);
-        int qid = root(q);
-        id[qid] = pid;
-    }
-
-    /**
-     * 带权重的union算法
-     *
-     * @param p
-     * @param q
-     */
-    public void weightedUnion(int p, int q) {
-        int pid = root(p);
-        int qid = root(q);
-        if (size[pid] >= size[qid]) {
+        int pid = root(p, id);
+        int qid = root(q, id);
+        if (pid == qid) {
+            return;
+        }
+        if (size[pid] > size[qid]) {
             id[qid] = pid;
             size[pid] += size[qid];
         } else {
             id[pid] = qid;
             size[qid] += size[pid];
         }
+        group--;
     }
 
-    /**
-     * 路径压缩的union算法
-     *
-     * @param p
-     * @param q
-     */
-    public void pathCompressionUnion(int p, int q) {
-        int pid = pathCompressionRoot(p);
-        int qid = pathCompressionRoot(q);
-        if (size[pid] >= size[qid]) {
-            id[qid] = pid;
-            size[pid] += size[qid];
-        } else {
-            id[pid] = qid;
-            size[qid] += size[pid];
-        }
-    }
-
-    /**
-     * 路径压缩的find Root 方法
-     *
-     * @param i
-     * @return
-     */
-    private int pathCompressionRoot(int i) {
-        while (i != id[i]) {
-            id[i] = id[id[i]];
-            i = id[i];
-        }
-        return i;
+    public int getGroup() {
+        return this.group;
     }
 }
