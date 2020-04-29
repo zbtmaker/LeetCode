6962dc0f8921b6755517509b6158e604563b334d
zoubaitao <zoubaitao@gmail.com>
2020-04-29 21:08:37 +0800

    optimize:LRUCache146,使用HashMap+双向链表方式实现，代码可参考Redis源码后再做优化
    Signed-off-by: zoubaitao <zoubaitao@gmail.com>

--- src/stack/LRUCache146.java
+++ src/stack/LRUCache146.java
@@ -3,49 +3,140 @@ package stack;
 import java.util.*;
 
 /**
+ * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。
+ * 获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。
+ * 写入数据 put(key, value) - 如果密钥已经存在，则变更其数据值；如果密钥不存在，则插入该组「密钥/数据值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+ * 链接：https://leetcode-cn.com/problems/lru-cache
+ *
  * @author baitao zou
  * date 2020/01/27
  */
 public class LRUCache146 {
-    private LinkedHashMap<Integer, Integer> map;
-    private int capacity;
+    /**
+     * 采用HashMap主要是我们需要查找实现O(1)的时间复杂度
+     */
+    private final Map<Integer, Node> map;
+    private final int capacity;
+    private int size;
+
+    /**
+     * 双向链表，主要是我们在链表更新节点的时候，如果是单节点，每次都需要
+     * 从头遍历找到当前节点的头节点，但是我们可以使用双向链表能够很快的得到
+     * 当前节点的上一个节点。
+     */
+    private Node head;
+    private Node tail;
 
     public LRUCache146(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("capacity argument exception");
+        }
+        this.size = 0;
         this.capacity = capacity;
-        this.map = new LinkedHashMap<>();
+        this.map = new HashMap<>(capacity);
     }
+
 
     /**
      * @param key 查找的Key
      * @return 存在返回value，不存在返回-1
      */
     public int get(int key) {
-        Integer val = map.get(key);
-        if (val == null) {
+        Node node = map.get(key);
+        if (node == null) {
             return -1;
         }
-        map.remove(key);
-        map.put(key, val);
-        return val;
+        refresh(node);
+        return node.val;
     }
 
-    public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            map.remove(key);
+    /**
+     * 更新当前节点的位置，判断头节点，尾节点以及中间节点三种情况，
+     *
+     * @param node 需要被更新的节点
+     */
+    private void refresh(Node node) {
+        if (node == head) {
+            return;
         }
-        map.put(key, value);
-        if (map.size() > capacity) {
-            Set<Map.Entry<Integer, Integer>> entrySet = map.entrySet();
-            int i = 0;
-            Integer tmpKey = null;
-            for (Map.Entry<Integer, Integer> entry : entrySet) {
-                if (i == 0) {
-                    tmpKey = entry.getKey();
-                    break;
-                }
-                i++;
-            }
-            map.remove(tmpKey);
+        if (node == tail) {
+            //更新尾节点
+            tail = tail.last;
+            tail.next = null;
+
+            //更新头节点
+            node.next = head;
+            head.last = node;
+            head = node;
+        } else {
+            //更新当前节点的上下节点
+            Node lastNode = node.last;
+            Node nextNode = node.next;
+            lastNode.next = nextNode;
+            nextNode.last = lastNode;
+
+            //更新头节点
+            node.last = null;
+            node.next = head;
+            head.last = node;
+            head = node;
+        }
+    }
+
+    /**
+     * put操作有一个值的注意的地方就是，如果key存在cache当中，此时需要操作两步，
+     * 第一步是更新当前key对应的value，于此同时还需要把这个节点更新到头节点上，如果
+     * 这个节点是尾节点还需要更新尾节点位置，所有的这些操作都由refresh操作完成。
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public void put(int key, int value) {
+        if (capacity <= 0) {
+            return;
+        }
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.val = value;
+            refresh(node);
+            return;
+        }
+        Node node = new Node(key, value);
+        if (head == null || tail == null) {
+            head = node;
+            tail = node;
+            size++;
+        } else if (size + 1 <= capacity) {
+            size++;
+            node.next = head;
+            head.last = node;
+            head = node;
+        } else {
+            size = capacity;
+            //更新头节点
+            node.next = head;
+            head.last = node;
+            head = node;
+
+            //删除HashMap中尾节点值
+            map.remove(tail.key);
+
+            //更新尾节点
+            tail = tail.last;
+            tail.next = null;
+        }
+        map.put(key, node);
+    }
+
+    private static class Node {
+        private final int key;
+        private int val;
+        private Node next;
+        private Node last;
+
+        private Node(int key, int val) {
+            this.key = key;
+            this.val = val;
         }
     }
 }
--- test/stack/LRUCache146Test.java
+++ test/stack/LRUCache146Test.java
@@ -12,30 +12,42 @@ import java.util.*;
  * date 2020/01/27
  */
 public class LRUCache146Test {
-    private LRUCache146 cache = new LRUCache146(2);
 
-    @Test
-    public void testLinkedHashMap() {
-        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
-        map.put(1, 1);
-        map.put(2, 2);
-        map.put(3, 3);
-        Set<Map.Entry<Integer, Integer>> entrySet = map.entrySet();
-        for(Map.Entry<Integer, Integer> entry : entrySet) {
-            System.out.println(entry.getKey() + "=" + entry.getValue());
-        }
-    }
 
     @Test
     public void test1() {
+        LRUCache146 cache = new LRUCache146(2);
         cache.put(1, 1);
         cache.put(2, 2);
         Assert.assertEquals(cache.get(1), 1);
         cache.put(3, 3);
-        Assert.assertEquals(cache.get(2), 2);
+        Assert.assertEquals(cache.get(2), -1);
         cache.put(4, 4);
         Assert.assertEquals(cache.get(1), -1);
         Assert.assertEquals(cache.get(3), 3);
         Assert.assertEquals(cache.get(4), 4);
+    }
+
+    @Test
+    public void test2() {
+        LRUCache146 cache = new LRUCache146(2);
+        Assert.assertEquals(cache.get(2), -1);
+        cache.put(2, 6);
+        Assert.assertEquals(cache.get(1), -1);
+        cache.put(1, 5);
+        cache.put(1, 2);
+        Assert.assertEquals(cache.get(1), 2);
+        Assert.assertEquals(cache.get(2), 6);
+    }
+
+    @Test
+    public void test3() {
+        LRUCache146 cache = new LRUCache146(2);
+        cache.put(2, 1);
+        cache.put(1, 1);
+        cache.put(2, 3);
+        cache.put(4, 1);
+        Assert.assertEquals(cache.get(1), -1);
+        Assert.assertEquals(cache.get(2), 3);
     }
 }
