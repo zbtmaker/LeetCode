108acc28badab747d7b09a3cc995720ff9770390
zoubaitao <zoubaitao@gmail.com>
2020-05-20 21:59:42 +0800

    optimize:CanPartition416，优化之前的代码，添加自顶向下递归的算法；自顶向下的动态规划算法还需要优化
    Signed-off-by: zoubaitao <zoubaitao@gmail.com>

--- src/math/CanPartition416.java
+++ src/math/CanPartition416.java
@@ -27,10 +27,19 @@ public class CanPartition416 {
      * 这样得到我们的递推关系是dp[i][j] = dp[i-1][j] || dp[i][j-nums[i]].
      * 3、得到递推关系式之后，我们还需要做的就是找出初始值，就像数学归纳法一样，由初始值推后面所有的递推关系式
      * 二、实现一个一维的动态规划（待优化）
+     *
      * @param nums
      * @return
      */
     public boolean canPartition(int[] nums) {
+        return canPartitionByDP(nums);
+    }
+
+    private boolean canPartitionByDP(int[] nums) {
+        return canPartitionByTopDownDynamicProgramming(nums);
+    }
+
+    private boolean canPartitionByTopDownDynamicProgramming(int[] nums) {
         if (nums == null || nums.length <= 1) {
             return false;
         }
@@ -40,19 +49,60 @@ public class CanPartition416 {
         }
         int target = sum / 2;
         int len = nums.length;
-        boolean[][] dp = new boolean[len][target + 1];
+        int[][] F = new int[len + 1][target + 1];
+        F[0][0] = 1;
+        for (int i = 1; i <= len; i++) {
+            for (int j = 1; j <= target; j++) {
+                F[i][j] = -1;
+            }
+        }
+        return recurCanPartitionByTopDownDynamicProgramming(nums, target, len, F) == 1;
+    }
+
+    private int recurCanPartitionByTopDownDynamicProgramming(int[] nums, int sum, int n, int[][] F) {
+        if (sum == 0 && n == 0) {
+            return 1;
+        }
+        if (n <= 0 || sum <= 0) {
+            return 0;
+        }
+        if (F[n][sum] >= 0) {
+            return F[n][sum];
+        }
+        if (sum < nums[n - 1]) {
+            F[n][sum] = F[n - 1][sum];
+            return F[n][sum];
+        }
+        F[n - 1][sum] = recurCanPartitionByTopDownDynamicProgramming(nums, sum, n - 1, F);
+        F[n - 1][sum - nums[n - 1]] = recurCanPartitionByTopDownDynamicProgramming(nums, sum - nums[n - 1], n - 1, F);
+        return F[n][sum] = F[n - 1][sum] | F[n - 1][sum - nums[n - 1]];
+    }
+
+    private boolean canPartitionByBottomUpDynamicProgramming(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        int sum = sum(nums);
+        if ((sum & 1) == 1) {
+            return false;
+        }
+        int target = sum >> 1;
+        int len = nums.length;
+        boolean[][] dp = new boolean[len + 1][target + 1];
         // 初始化成为 true 虽然不符合状态定义，但是从状态转移来说是完全可以的
         dp[0][0] = true;
+        for (int i = 1; i <= target; i++) {
 
-        if (nums[0] <= target) {
-            dp[0][nums[0]] = true;
+            dp[0][i] = false;
         }
-
-        for (int i = 1; i < len; i++) {
-            for (int j = 0; j <= target; j++) {
+        for (int j = 1; j <= len; j++) {
+            dp[j][0] = false;
+        }
+        for (int i = 1; i <= len; i++) {
+            for (int j = 1; j <= target; j++) {
                 dp[i][j] = dp[i - 1][j];
-                if (nums[i] <= j) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+                if (nums[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
                 }
             }
             // 由于状态转移方程的特殊性，提前结束，可以认为是剪枝操作
@@ -60,7 +110,85 @@ public class CanPartition416 {
                 return true;
             }
         }
-        return dp[len - 1][target];
+        return dp[len][target];
+    }
+
+    /**
+     * 回溯算法，对于回溯算法
+     *
+     * @param nums
+     * @return
+     */
+    private boolean canPartitionByBacktracking(int[] nums) {
+        return canPartitionByTopDownBacktracking(nums);
+    }
+
+    /**
+     * 自顶向下回溯
+     *
+     * @param nums
+     * @return
+     */
+    private boolean canPartitionByTopDownBacktracking(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        int sum = sum(nums);
+        if ((sum & 1) == 1) {
+            return false;
+        }
+        return recurCanPartitionByTopDownBacktracking(nums, sum >> 1, nums.length);
+    }
+
+
+    private boolean recurCanPartitionByTopDownBacktracking(int[] nums, int sum, int n) {
+        if (sum == 0) {
+            return true;
+        }
+        if (sum < 0 || n == 0) {
+            return false;
+        }
+
+        return recurCanPartitionByTopDownBacktracking(nums, sum, n - 1) || recurCanPartitionByTopDownBacktracking(nums, sum - nums[n - 1], n - 1);
+    }
+
+    /**
+     * 自底向上回溯
+     *
+     * @param nums
+     * @return
+     */
+    private boolean canPartitionByBottomUpBacktracking(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        int sum = sum(nums);
+        if ((sum & 1) == 1) {
+            return false;
+        }
+        return recurCanPartitionByTopDownBacktracking(nums, sum >> 1, nums.length);
+    }
+
+    /**
+     * 递归解决该问题，递归的方式解决这个问题，我们从0开始计算，如果我们的target == 0，那么表示数组总存在一个有效的子数组使得
+     *
+     * @param nums
+     * @param index
+     * @param target
+     * @return
+     */
+    private boolean recurCanPartitionByBottomUpBacktracking(int[] nums, int index, int target) {
+        if (target == 0) {
+            return true;
+        }
+        for (int i = index; i < nums.length; i++) {
+            target -= nums[i];
+            if (recurCanPartitionByBottomUpBacktracking(nums, i + 1, target)) {
+                return true;
+            }
+            target += nums[i];
+        }
+        return false;
     }
 
     /**
