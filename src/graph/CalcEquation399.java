04580e201a22cfd91af92f39b3e67515cf806d3e
zoubaitao <zoubaitao@gmail.com>
2020-11-27 22:41:19 +0800

    add:CalcEquation399，采用DFS方式计算，之前用的BFS，后面采用Union Find方式优化
    Signed-off-by: zoubaitao <zoubaitao@gmail.com>

--- src/graph/CalcEquation399.java
+++ src/graph/CalcEquation399.java
@@ -9,24 +9,25 @@ import java.util.*;
  */
 public class CalcEquation399 {
 
-
-    public class Edge {
-        String dividend;
-        double result;
-
-        private Edge(String dividend, double result) {
-            this.dividend = dividend;
-            this.result = result;
-        }
+    /**
+     * @param equations 已知方程式
+     * @param values    方程式结果值
+     * @param queries   需要查询的方程式
+     * @return 等式结果值
+     */
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        return calcEquationByDFS(equations, values, queries);
     }
 
     /**
+     * BFS 方式求解
+     *
      * @param equations 已知方程式
      * @param values    方程式结果值
      * @param queries   需要查询的方程式
      * @return
      */
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+    private double[] calcEquationByBFS(List<List<String>> equations, double[] values, List<List<String>> queries) {
         if (queries == null || queries.size() == 0) {
             return new double[0];
         }
@@ -126,5 +127,111 @@ public class CalcEquation399 {
             tmp = edge.dividend;
         }
         return result;
+    }
+
+    public class Edge {
+        String dividend;
+        double result;
+
+        private Edge(String dividend, double result) {
+            this.dividend = dividend;
+            this.result = result;
+        }
+    }
+
+    /**
+     * 通过DFS算法寻找图中的一条路径
+     *
+     * @param equations 等式左边数组
+     * @param values    等式右边结果数组
+     * @param queries   待查询等式
+     * @return 等式结果数组
+     */
+    public double[] calcEquationByDFS(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        Map<String, List<Node>> graph = constructGraph(equations, values);
+        int len = queries.size();
+        double[] result = new double[len];
+        for (int i = 0; i < len; i++) {
+            List<String> query = queries.get(i);
+            String start = query.get(0);
+            String end = query.get(1);
+            if (graph.get(start) == null || graph.get(end) == null) {
+                result[i] = -1;
+            } else if (end.equals(start)) {
+                result[i] = 1;
+            } else {
+                result[i] = dfs(start, end, graph, new HashSet<>(), 1d);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @param start 起始点
+     * @param end   终点
+     * @param graph 邻接表
+     * @param flag  遍历集合
+     * @param value 上一步运算结果
+     * @return -1-不存在 ｜ 运算结果
+     */
+    private double dfs(String start, String end, Map<String, List<Node>> graph, Set<String> flag, double value) {
+        flag.add(start);
+        List<Node> edges = graph.get(start);
+        if (edges == null) {
+            return -1d;
+        }
+        for (Node node : edges) {
+            if (flag.contains(node.des)) {
+                continue;
+            }
+            if (node.getDes().equals(end)) {
+                return value * node.getVal();
+            }
+            double result = dfs(node.getDes(), end, graph, flag, value * node.getVal());
+            if (result != -1) {
+                return result;
+            }
+        }
+        return -1d;
+    }
+
+    /**
+     * @param equations 等式左边数组
+     * @param values    等式右边结果数组
+     * @return 邻接表
+     */
+    private Map<String, List<Node>> constructGraph(List<List<String>> equations, double[] values) {
+        Map<String, List<Node>> graph = new HashMap<>();
+
+        for (int i = 0; i < equations.size(); i++) {
+            List<String> equation = equations.get(i);
+            String divisor = equation.get(0);
+            String dividend = equation.get(1);
+            double value = values[i];
+            List<Node> edges1 = graph.computeIfAbsent(divisor, k -> new ArrayList<>());
+            edges1.add(new Node(dividend, value));
+
+            List<Node> edges2 = graph.computeIfAbsent(dividend, k -> new ArrayList<>());
+            edges2.add(new Node(divisor, 1 / value));
+        }
+        return graph;
+    }
+
+    public static class Node {
+        private String des;
+        private Double val;
+
+        public Node(String des, Double val) {
+            this.des = des;
+            this.val = val;
+        }
+
+        public String getDes() {
+            return this.des;
+        }
+
+        public Double getVal() {
+            return this.val;
+        }
     }
 }
