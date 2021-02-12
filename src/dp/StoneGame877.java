b4a86f589363d3992815cca5044c119b2285239c
zoubaitao <zoubaitao@gmail.com>
2021-02-12 16:44:01 +0800

    feat: 优化StoneGame877, 采用negamax算法

--- src/dp/StoneGame877.java
+++ src/dp/StoneGame877.java
@@ -1,5 +1,7 @@
 package dp;
 
+import java.util.Arrays;
+
 /**
  * 亚历克斯和李用几堆石子在做游戏。偶数堆石子排成一行，每堆都有正整数颗石子 piles[i] 。
  * 游戏以谁手中的石子最多来决出胜负。石子的总数是奇数，所以没有平局。
@@ -11,6 +13,9 @@ package dp;
  * date 2020/04/13
  */
 public class StoneGame877 {
+    private int maxVal = Integer.MIN_VALUE;
+    private int minVal = Integer.MAX_VALUE;
+
     static class Pair {
         //先手
         int fir;
@@ -32,22 +37,27 @@ public class StoneGame877 {
      * 如果先手选择完了，如果先手选择完了左边，那么先手就是选择了piles(i)，那么sec就只能在(i+1,j)最大化了。如果fir选择了piles(j)，那么sec就只能
      * 在(i,j-1)内取得最大值。因此fir和sec的递推关系如下：
      * dp(i,j).fir = max{piles(i))+dp(i+1,j).sec, piles(j)+dp(i,j-1).sec}
-     *  if(piles(i) + dp(i+1,j).sec > piles(j) + dp(i,j-1).sec)
+     * if(piles(i) + dp(i+1,j).sec > piles(j) + dp(i,j-1).sec)
      * dp(i,j).sec = dp(i+1,j).fir
-     *  if(piles(i) + dp(i+1,j).sec <= piles(j) + dp(i,j-1).sec)
+     * if(piles(i) + dp(i+1,j).sec <= piles(j) + dp(i,j-1).sec)
      * dp(i,j).sec = dp(i+1,j).fir
      * <p>
      * 2、但是如果i==j的时候，表示堆的长度为1，那么这个时候能够拿到堆piles(i)的长度dp(i,i).fir = piles(i)，因此后手拿不到任何堆，因此dp(i,i).sec=0
      * <p>
      * 3、我们要求dp(0,len-1),会发现求dp(0,len-1),要以来dp(1,len - 1)和dp(0,len-2)。所以我们需要从矩阵
      * 中从左往右求解，从下往上求解矩阵中每一个值
-     *
+     * <p>
      * 4、最后就只需要判断dp(0,len-1).fir >= dp(0,len).sec
+     * 其实这种方式实现是对的，但是当你理解这个问题来还是有点障碍，其实问题确实可以理解成前后手问题，但是前后手问题我的理解是无数种选择造就了
      *
      * @param piles
      * @return
      */
     public boolean stoneGame(int[] piles) {
+        return stoneGameNegamax(piles);
+    }
+
+    private boolean stoneGameByUpDown(int[] piles) {
         int len = piles.length;
         Pair[][] dp = new Pair[len][len];
         for (int i = 0; i < len; i++) {
@@ -69,5 +79,38 @@ public class StoneGame877 {
             }
         }
         return dp[0][len - 1].fir > dp[0][len - 1].sec;
+    }
+
+    /**
+     * 这个问题是一个Zero-sum game，理论上是可以用minmax算法解决问题，但是根据wiki中采用negamax算法可以解决
+     * 问题。我根据negamax算法的理解就是maxPlayer的最大值，然后minPlayer的最小值，其实就是可以理解为maxPlayer - minPlayer
+     * 我们要找到这个差值的最大值。对于zero-sum game就是给定一个utility function，这个utility function其实是MaxPlayer的，但是
+     * minPlayer的目标是就是最小化这个maxPlayer的utility function。
+     *
+     * @param piles 石头堆
+     * @return 最大石头堆
+     */
+    private boolean stoneGameNegamax(int[] piles) {
+        int len = piles.length;
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+        int result = negamax(piles, 0, len - 1, dp);
+        int sum = Arrays.stream(piles).sum();
+        int maxPlayer = (result + sum) >> 1;
+        return maxPlayer > sum - maxPlayer;
+    }
+
+    private int negamax(int[] piles, int left, int right, int[][] dp) {
+        if (left > right) {
+            return 0;
+        }
+        if (dp[left][right] != Integer.MAX_VALUE) {
+            return dp[left][right];
+        }
+        dp[left][right] = Math.max(piles[left] - negamax(piles, left + 1, right, dp),
+                piles[right] - negamax(piles, left, right - 1, dp));
+        return dp[left][right];
     }
 }
