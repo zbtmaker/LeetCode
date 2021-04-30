f9ebf96f21d094ee955de3b26a0a36ca1fb08428
zoubaitao <zoubaitao@gmail.com>
2021-04-30 08:58:00 +0800

    feat: optimize Caculate227
    Signed-off-by: zoubaitao <zoubaitao@gmail.com>

--- src/stack/Calculate227.java
+++ src/stack/Calculate227.java
@@ -8,6 +8,7 @@ import java.util.Stack;
  * date 2020/01/25
  */
 public class Calculate227 {
+
     /**
      * 这个问题主要是使用String的replace(" ")方法和Stack。
      *
@@ -15,87 +16,80 @@ public class Calculate227 {
      * @return
      */
     public int calculate(String s) {
-        String str = replaceBlank(s);
-        if (str.length() == 0) {
-            return 0;
-        }
-        int len = str.length();
         Stack<Integer> numStack = new Stack<>();
-        Stack<Character> strStack = new Stack<>();
-        int i = 0;
-        while (i < len) {
-            int k = i + 1;
-            while (k < len && str.charAt(k) >= '0' && str.charAt(k) <= '9') {
-                k++;
+        int len = s.length(), num = 0;
+        char ch;
+        char sign = '+';
+        for (int i = 0; i < len; i++) {
+            ch = s.charAt(i);
+            if (ch >= '0' && ch <= '9') {
+                num = num * 10 + (s.charAt(i) - '0');
             }
-            String subString = str.substring(i, k);
-            int curNum = Integer.parseInt(subString);
-            if (!strStack.isEmpty()) {
-                char ch = strStack.peek();
-                if (ch == '*') {
-                    strStack.pop();
-                    int preNum = numStack.pop();
-                    numStack.push(preNum * curNum);
-                } else if (ch == '/') {
-                    strStack.pop();
-                    int preNum = numStack.pop();
-                    numStack.push(preNum / curNum);
-                } else if (ch == '-') {
-                    numStack.push(-curNum);
-                    strStack.pop();
-                    strStack.push('+');
-                } else {
-                    numStack.push(curNum);
+            if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || i == len - 1) {
+                if (sign == '+') {
+                    numStack.push(num);
+                } else if (sign == '-') {
+                    numStack.push(-num);
+                } else if (sign == '*') {
+                    numStack.push(numStack.pop() * num);
+                } else if (sign == '/') {
+                    numStack.push(numStack.pop() / num);
                 }
-            } else {
-                numStack.push(curNum);
-            }
-
-            i = k;
-            if (i >= len) {
-                break;
-            }
-            char ch = str.charAt(i);
-            strStack.push(ch);
-            i++;
-        }
-        if (strStack.isEmpty()) {
-            return numStack.pop();
-        }
-        while (!numStack.isEmpty() && !strStack.isEmpty()) {
-            char character = strStack.pop();
-            int curNum = numStack.pop();
-            int preNum = numStack.pop();
-            if (character == '+') {
-                numStack.push(curNum + preNum);
-            } else if (character == '-') {
-                numStack.push(preNum - curNum);
-            } else if (character == '*') {
-                numStack.push(preNum * curNum);
-            } else if (character == '/') {
-                numStack.push(preNum / curNum);
+                sign = ch;
+                num = 0;
             }
         }
-        return numStack.pop();
+        int result = 0;
+        while (!numStack.isEmpty()) {
+            result += numStack.pop();
+        }
+        return result;
     }
 
-    /**
-     * 这个方法用来取代字符串中的Blank
-     *
-     * @param str 字符串
-     * @return 无空字符的字符串
-     */
-    private String replaceBlank(String str) {
-        if (str == null || str.length() == 0) {
-            return "";
-        }
-        StringBuilder sb = new StringBuilder(str.length());
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (ch != ' ') {
-                sb.append(ch);
+    public int calculateI(String s) {
+        int curNum = 0, lastNum = 0, result = 0;
+        char lastOperator = '+';
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch >= '0' && ch <= '9') {
+                curNum = curNum * 10 + (ch - '0');
+            } else if(ch == '+' || ch == '-') {
+                if(lastOperator == '+') {
+                    result += curNum;
+                } else if(lastOperator == '-') {
+                    result -= curNum;
+                } else if(lastOperator == '*') {
+                    result += lastNum * curNum;
+                } else {
+                    result += lastNum / curNum;
+                }
+                curNum = 0;
+                lastOperator = ch;
+                lastNum = 0;
+            } else if(ch == '*' || ch == '/') {
+                if(lastOperator == '+') {
+                    lastNum = curNum;
+                } else if(lastOperator == '-') {
+                    lastNum = -curNum;
+                } if(lastOperator == '*') {
+                    lastNum = lastNum * curNum;
+                } else if(lastOperator == '/') {
+                    lastNum = lastNum / curNum;
+                }
+                curNum = 0;
+                lastOperator = ch;
             }
+            //空字符串不做处理
         }
-        return sb.toString();
+        if(lastOperator == '+') {
+            result += curNum;
+        } else if(lastOperator == '-') {
+            result -= curNum;
+        } else if(lastOperator == '*') {
+            result += lastNum * curNum;
+        } else {
+            result += lastNum / curNum;
+        }
+        return result;
     }
 }
--- test/stack/Calculate227Test.java
+++ test/stack/Calculate227Test.java
@@ -1,5 +1,6 @@
 package stack;
 
+import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,65 +8,67 @@ import org.junit.Test;
  * @author baitao zou
  * date 2020/01/25
  */
-public class Calculate227Test {
+public class Calculate227Test extends TestCase {
     private Calculate227 calculate = new Calculate227();
 
-    @Test
     public void test1() {
         String s = "3+2*2";
-        int result = calculate.calculate(s);
+        int result = calculate.calculateI(s);
         Assert.assertEquals(result, 7);
     }
 
 
-    @Test
     public void test2() {
         String s = "3+12*2";
-        int result = calculate.calculate(s);
+        int result = calculate.calculateI(s);
         Assert.assertEquals(result, 27);
     }
 
 
-    @Test
     public void test3() {
         String s = " 3+ 12/ 2 * 4";
-        int result = calculate.calculate(s);
+        int result = calculate.calculateI(s);
         Assert.assertEquals(result, 27);
     }
 
-    @Test
     public void test4() {
         String s = " 3+ 5 / 2";
-        int result = calculate.calculate(s);
+        int result = calculate.calculateI(s);
         Assert.assertEquals(result, 5);
     }
 
-    @Test
     public void test5() {
         String s = " 5 / 2";
-        int result = calculate.calculate(s);
+        int result = calculate.calculateI(s);
         Assert.assertEquals(result, 2);
     }
 
 
-    @Test
     public void test6() {
         String s = " 5 ";
-        int result = calculate.calculate(s);
+        int result = calculate.calculateI(s);
         Assert.assertEquals(result, 5);
     }
 
-    @Test
-    public void test7 (){
+    public void test7() {
         String str = "1-1+ 1";
-        int result = calculate.calculate(str);
+        int result = calculate.calculateI(str);
         Assert.assertEquals(result, 1);
     }
 
-    @Test
-    public void test8 (){
+    public void test8() {
         String str = "1-1*8/4*2+ 1";
-        int result = calculate.calculate(str);
+        int result = calculate.calculateI(str);
         Assert.assertEquals(result, -2);
+    }
+
+    public void test9() {
+        int result = calculate.calculateI("0-2147483647");
+        Assert.assertEquals(result, -2147483647);
+    }
+
+    public void test10() {
+        int result = calculate.calculateI(" -3+5 / 2 + 1");
+        Assert.assertEquals(result, 0);
     }
 }
