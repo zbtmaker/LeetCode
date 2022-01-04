91ec24f653c202f8654628727d6ff5d7fe1df44f
zoubaitao <zoubaitao@gmail.com>
2022-01-05 00:32:32 +0800

    fix: 优化FairCandySwap888

--- src/array/FairCandySwap888.java
+++ src/array/FairCandySwap888.java
@@ -1,8 +1,6 @@
 package array;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 
 /**
  * Created by Administrator on 2019\1\19 0019.
@@ -11,29 +9,28 @@ public class FairCandySwap888 {
 
     public int[] fairCandySwap(int[] A, int[] B) {
         int sumA = 0;
-        HashMap<Integer, List<Integer>> aValueMapIndex = new HashMap<>();
-        for (int i = 0; i < A.length; i++) {
-            sumA += A[i];
-            List<Integer> list = aValueMapIndex.computeIfAbsent(A[i], k -> new ArrayList<>());
-            list.add(i);
+        for (int j : A) {
+            sumA += j;
         }
 
         int sumB = 0;
-        HashMap<Integer, List<Integer>> bValueMapIndex = new HashMap<>();
-        for (int i = 0; i < B.length; i++) {
-            sumB += B[i];
-            List<Integer> list = bValueMapIndex.computeIfAbsent(B[i], k -> new ArrayList<>());
-            list.add(i);
+        HashSet<Integer> bValueSet = new HashSet<>();
+        for (int j : B) {
+            sumB += j;
+            bValueSet.add(j);
         }
 
         int average = (sumA + sumB) >> 1;
 
         int[] result = new int[2];
         for (int i = 0; i < A.length; i++) {
-            List<Integer> indexList = bValueMapIndex.get(sumA - A[i] - average);
-            if (indexList != null) {
-                result[0] = i;
-                result[1] = indexList.get(0);
+            int valA = sumA - A[i];
+            if (valA > average) {
+                continue;
+            }
+            if (bValueSet.contains(average - valA)) {
+                result[0] = A[i];
+                result[1] = average - valA;
                 return result;
             }
         }
--- test/array/FairCandySwap888Test.java
+++ test/array/FairCandySwap888Test.java
@@ -13,21 +13,21 @@ public class FairCandySwap888Test extends TestCase {
 
     public void test1() {
         int[] result = candySwap.fairCandySwap(new int[]{1, 1}, new int[]{2, 2});
-        Assert.assertArrayEquals(result, new int[]{0, 0});
+        Assert.assertArrayEquals(result, new int[]{1, 2});
     }
 
     public void test2() {
         int[] result = candySwap.fairCandySwap(new int[]{1, 2}, new int[]{2, 3});
-        Assert.assertArrayEquals(result, new int[]{1, 1});
+        Assert.assertArrayEquals(result, new int[]{1, 2});
     }
 
     public void test3() {
         int[] result = candySwap.fairCandySwap(new int[]{2}, new int[]{1, 3});
-        Assert.assertArrayEquals(result, new int[]{0, 1});
+        Assert.assertArrayEquals(result, new int[]{2, 3});
     }
 
     public void test4() {
         int[] result = candySwap.fairCandySwap(new int[]{1, 2, 5}, new int[]{2, 4});
-        Assert.assertArrayEquals(result, new int[]{2, 1});
+        Assert.assertArrayEquals(result, new int[]{5, 4});
     }
 }
