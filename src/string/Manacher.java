32ae974c8edbec0c6571124e0aa2e9cd2f68fe86
zoubaitao <zoubaitao@gmail.com>
2022-10-20 23:13:03 +0800

    feat: 优化Manacher算法

--- src/string/Manacher.java
+++ src/string/Manacher.java
@@ -1,7 +1,5 @@
 package string;
 
-import java.util.Arrays;
-
 /**
  * Created by Administrator on 2019\1\22 0022.
  * Manacher 算法主要是为了解决字符串中的回文串判断问题
@@ -24,70 +22,31 @@ import java.util.Arrays;
  * VersionII 是在VersionI的改进版本，将两种情况(I)和(II-c)进行合并，将(II-a)和(II-b)进行合并
  */
 public class Manacher {
-
-
+    /**
+     * 我们对于每一个给定的字符串，第一步做的事情就是预处理，在字符串的头部和尾部都添加一个"#"字符，同时每个字符串
+     * 中间也应该添加一个字符"#"。例如字符串"aba"，预处理之后就变成了"#a#b#a#"
+     * 我们用三个指针来维护当前位置的最长的回文字符串，L-回文左边界，R-回文右边界，C-下一个回文的中心位置。如果一个
+     * 用指针i表示当前位置。如果当前位置的回文右边界超出了之前的回文有边界，那么需要更新C（下一个回文的中心位置）。
+     * I、如果当前位置不在[L,R]范围内，那么采用朴素的方式判断以当前位置为核心的
+     *
+     * @param s
+     * @return
+     */
     public static int[] manacher(String s) {
         StringBuilder sb = preManacher(s);
-        return manacherII(sb);
+        return manacher(sb);
     }
 
     public static int[] manacher(StringBuilder sb) {
-        return manacherII(sb);
-    }
-
-    private static int[] manacherI(StringBuilder sb) {
-        int size = sb.length();
-        int[] radius = new int[size];
-        Arrays.fill(radius, 1);
-        int R = -1;
-        int center = -1;
-        for (int i = 0; i < size; i++) {
-            radius[i] = 1;
-            int count = radius[i];
-            if (i > R) {
-                int left = i - 1;
-                int right = i + 1;
-                while (left >= 0 && right < size && sb.charAt(left) == sb.charAt(right)) {
-                    count++;
-                    left--;
-                    right++;
-                }
-                radius[i] = count;
-                center = i;
-                R = Math.max((right - 1), R);
-            } else {
-                if (i + radius[2 * center - i] - 1 == R) {
-                    count = radius[2 * center - i];
-                    int left = i - radius[2 * center - i];
-                    int right = i + radius[2 * center - i];
-                    while (left >= 0 && right < size && sb.charAt(left) == sb.charAt(right)) {
-                        count++;
-                        left--;
-                        right++;
-                    }
-                    radius[i] = count;
-                    if (R < right - 1) {
-                        center = i;
-                        R = right - 1;
-                    }
-
-                } else if (i + radius[2 * center - i] - 1 > R) {
-                    radius[i] = R - i + 1;
-                } else {
-                    radius[i] = radius[2 * center - i];
-                }
-            }
-        }
-        return radius;
-    }
-
-    private static int[] manacherII(StringBuilder sb) {
         int size = sb.length();
         int[] radius = new int[sb.length()];
         int R = -1;
         int center = -1;
         for (int i = 0; i < size; i++) {
             radius[i] = 1;
+            // 指针i不在上一个回文范围内，此时采用朴素的方式计算当前位置为核心的回文
+            // 如果i的镜像位置2*center-i的半径Radius[2*center - i] - 1 + i 的位置刚好处在上一个回文的边界位置,
+            // 同样也是采用朴素的方式计算当前位置的回文半径
             if (i > R || i + radius[2 * center - i] - 1 == R) {
                 int temp = i > R ? i : center;
                 int count = radius[2 * temp - i];
@@ -104,8 +63,13 @@ public class Manacher {
                     R = right - 1;
                 }
             } else if (i + radius[2 * center - i] - 1 > R) {
+                // 如果当前指针在上一个回文半径内部，但是i的镜像位置的回文半径超出了L，那么i位置的回文半径必定是i~R区间的的长度
+                // 这里我们可以使用反证法，假如i位置的回文半径也超出了R，那么前一个回文有边界将不再是R，而是另外一个参数，因为
+                // 根据对称特性可以得知。因此就可以得到i位置的回文半径。
                 radius[i] = R - i + 1;
             } else {
+                // 如果当前指针在上一个回文半径内部，且当前位置i+radius[2*center-i] - 1 < R，那么就不需要重复计算当前位置
+                // 的回文半径，这个是因为镜像位置都是对称的，所以不需要计算了。
                 radius[i] = radius[2 * center - i];
             }
         }
